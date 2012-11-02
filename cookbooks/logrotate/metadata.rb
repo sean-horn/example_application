a68a9425343b0574bc1383929f921279d4ad9f6d
Sean Horn <sean_horn@opscode.com>
2012-11-02 13:15:18 -0400

    Import logrotate version 1.0.2

---
+++ cookbooks/logrotate/metadata.rb
@@ -0,0 +1,13 @@
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs logrotate package and provides a definition for logrotate configs"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.2"
+
+recipe "logrotate", "Installs logrotate package"
+provides "logrotate_app"
+
+%w{ redhat centos debian ubuntu scientific amazon fedora }.each do |os|
+  supports os
+end
